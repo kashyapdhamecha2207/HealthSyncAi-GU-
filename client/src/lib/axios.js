93a59978fca167b52dda6ecb9187da3527a7de49
HealthSync AI+ <healthsync@example.com>
2026-04-04 10:14:05 +0530

    Update API baseURL to deployed backend

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://healthsyncai-gu.onrender.com/api',
 });
 
 // Interceptor to add auth token
